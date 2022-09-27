7f211260c55f9e318919ae990577d955620b9cbf
Ulengin <furkanulengin@gmail.com>
2022-09-27 23:11:19 +0300

    IAB_OdevBir

---
+++ BirinciOdevFurkanUlengin/app/src/main/java/com/example/birinciodevfurkanulengin/OdevBirFurkanUlengin/odevBir.kt
@@ -0,0 +1,53 @@
+package com.example.birinciodevfurkanulengin.OdevBirFurkanUlengin
+
+fun main() {
+    val islemNo = 123456
+    val ogrNo = 1721231006
+    val kitapNo = 32425
+    val aTarih = "12.03.2022"
+    val vTarih = "13.07.2022"
+
+    println("islem No: $islemNo")
+    println("Ogrenci No : $ogrNo")
+    println("Kitap No: $kitapNo")
+    println("Alım Tarihi: $aTarih")
+    println("Veriliş Tarihi :$vTarih")
+
+    val ogrdAd = "Furkan"
+    val ogrSoyad = "ÜLENGİN"
+    val cinsiyet = "Erkek"
+    val dTarih = "28.08.1999"
+    val sinif = 4
+
+    println("Ogrenci Adı: $ogrdAd")
+    println("Ogrenci Soyadı :$ogrSoyad")
+    println("Ogrenci Cinsiyeti :$cinsiyet")
+    println("Ogrenci Doğum Tarihi: $dTarih")
+    println("Sınıf: $sinif")
+
+    val isbnNo = 867676
+    val kitapAdi = "Don Kişot"
+    val yazarNo = 142
+    val turNo = 2
+    val sayfaSayisi = 235
+    val puan = 9.8
+
+    println("isbn No : $isbnNo")
+    println("Kitap Adı : $kitapAdi")
+    println("Yazar No: $yazarNo")
+    println("Tür No: $turNo")
+    println("Sayfa Sayısı : $sayfaSayisi")
+    println("Puan : $puan")
+
+
+    val yazarAd = "Miguel"
+    val yazarSoyad = "de Cervantes"
+
+    println("Yazar Adı: $yazarAd")
+    println("Yazar Soyadı : $yazarSoyad")
+
+    val turAdi = "Macera"
+
+    println("Tür Adı: $turAdi")
+
+}
